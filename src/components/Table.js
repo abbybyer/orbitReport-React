253b00b3238e9683cd417bf7d33634aba93751dc
unknown <abbynb15@gmail.com>
2023-07-14 22:49:03 -0500

    Did all of assignment 6 coding and styling, pre-autograde

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,37 @@
+// Need to correspond the value of each <td> tag to the <th> tag (i.e. link titles to data in that column)
+
+import './styling.css';
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
-        <tr>
-          <th>Header TBD</th>
-        </tr>
+          <tr>
+            <th>Name</th>
+            <th>Type of Satelite</th>
+            <th>Launch Date</th>
+            <th>Status</th>
+          </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data, id) => {
+            let statusDisp = () => {
+              if(data.operational === true){
+                return "Active";
+              }else if(data.operational === false){
+                return "Inactive";
+              }
+            }
+
+            return(
+              <tr key={id}>
+                <td>{data.name}</td>
+                <td>{data.type}</td>
+                <td>{data.launchDate}</td>
+                  <td>{statusDisp()}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
   );
